8284689d6ceeda767a96de3cb63eb5f1b37851ab
ilya096 <kritsky09@gmail.com>
2026-06-07 23:55:48 +0300

    fix: simplify HallEnvironment for black screen debug

--- src/components/HallEnvironment.tsx
+++ src/components/HallEnvironment.tsx
@@ -20,25 +20,15 @@ function Exhibit({ position, id }: { position: [number, number, number], id: str
 }
 
 export const HallEnvironment: React.FC = () => {
-  const { posters } = usePosterStore();
-  
   return (
     <>
       <Canvas camera={{ position: [0, 1.6, 5] }} style={{ width: '100%', height: '100%' }}>
+        <color attach="background" args={['#222']} />
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
         <Exhibit position={[0, 0.5, 0]} id='exhibit-1' />
         <Exhibit position={[-2, 0.5, 0]} id='exhibit-2' />
         <Exhibit position={[2, 0.5, 0]} id='exhibit-3' />
-        {posters.map((poster) => (
-          <Poster
-            key={poster.id}
-            position={poster.position}
-            rotation={poster.rotation}
-            scale={poster.scale}
-            texturePath={poster.texturePath}
-          />
-        ))}
       </Canvas>
       <InfoPanel />
     </>
